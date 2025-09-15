71cc2751219b31d231e43c7fd154c2394b882599
kaigidwani <kpg8244@rit.edu>
2025-09-14 23:46:51 -0400

    Fixed wrong character in htmlResonses

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const index = fs.readFileSync(`%{__dirname}/../client/client.html`);
-const page2 = fs.readFileSync(`%{__dirname}/../client/client2.html`);
-const page3 = fs.readFileSync(`%{__dirname}/../client/client3.html`);
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
